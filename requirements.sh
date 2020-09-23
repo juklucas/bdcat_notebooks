3bec56cdde5707818d3c8907af9d34a3fe992b2a
aofarrel <aofarrel@ucsc.edu>
2020-09-23 14:44:29 -0700

    add requirements.sh

---
+++ requirements.sh
@@ -0,0 +1,6 @@
+# Only run inside a Python 3.7 venv!
+pip install flake8
+pip install mypy
+pip install firecloud
+pip install git+https://github.com/xbrianh/callysto
+pip install terra-notebook-utils
